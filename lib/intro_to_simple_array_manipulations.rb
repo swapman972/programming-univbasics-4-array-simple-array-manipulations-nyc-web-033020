b7dfb80c4f7f75d54f482b803bf6035f77f8e5cc
Jordan Melidor-Fuxis <jordan.melidor@hotmail.fr>
2020-03-21 17:31:16 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,37 @@
+def using_push(array, element)
+  array.push(element)
+end
+
+def using_unshift(array, string)
+  array.unshift(string)
+end
+
+def using_pop
+end
+
+def pop_with_args
+end
+
+def using_shift
+end
+
+def shift_with_args
+end
+
+def using_concat
+end
+
+def using_insert
+end
+
+def using_uniq
+end
+
+def using_flatten
+end
+
+def using_delete
+end
+
+def using_delete_at
+end
